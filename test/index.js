73add5b462fa4ef359dd13ed2b29faa0fde8b055
Paul OFallon <paul@Pauls-Mac-mini.local>
2015-04-23 13:10:40 -0400

    Formatting

--- test/index.js
+++ test/index.js
@@ -5,86 +5,85 @@ var collections = require('metalsmith-collections');
 
 describe('metalsmith-collection-scoping', function() {
 
-	it('should remove private collections with no scope option', function(done) {
-	  var metalsmith = Metalsmith('test/fixtures/basic');
-		metalsmith
-		  .use(collections({
-        articles: {},
-        secrets: {
-          metadata: { private: true }
-        }
-      }))
-      .use(collectionScoping())
-		  .build(function(err) {
-		  	if (err) return(done(err));
-		  	var m = metalsmith.metadata();
-		  	assert.notEqual(m.articles,null);
-		  	assert.notEqual(m.collections.articles,null);
-		  	assert.equal(m.secrets,null);
-		  	assert.equal(m.collections.secrets,null);
-		  	done();
-		  });
-	});
+    it('should remove private collections with no scope option', function(done) {
+      var metalsmith = Metalsmith('test/fixtures/basic');
+        metalsmith
+        .use(collections({
+            articles: {},
+            secrets: {
+                metadata: { private: true }
+            }
+        }))
+        .use(collectionScoping())
+        .build(function(err) {
+            if (err) return(done(err));
+            var m = metalsmith.metadata();
+            assert.notEqual(m.articles,null);
+            assert.notEqual(m.collections.articles,null);
+            assert.equal(m.secrets,null);
+            assert.equal(m.collections.secrets,null);
+            done();
+        });
+    });
 
-	it('should remove private collections with non-private scope option', function(done) {
-		var metalsmith = Metalsmith('test/fixtures/basic');
-		metalsmith
-	  	.use(collections({
-        articles: {},
-        secrets: {
-          metadata: { private: true }
-        }
-     	}))
-      .use(collectionScoping({scope: 'public'}))
-		  .build(function(err) {
-		  	if (err) return(done(err));
-		  	var m = metalsmith.metadata();
-		  	assert.notEqual(m.articles,null);
-		  	assert.notEqual(m.collections.articles,null);
-		  	assert.equal(m.secrets,null);
-		  	assert.equal(m.collections.secrets,null);
-		  	done();
-		  });
-	});
+    it('should remove private collections with non-private scope option', function(done) {
+        var metalsmith = Metalsmith('test/fixtures/basic');
+        metalsmith
+        .use(collections({
+            articles: {},
+            secrets: {
+                metadata: { private: true }
+            }
+        }))
+        .use(collectionScoping({scope: 'public'}))
+        .build(function(err) {
+            if (err) return(done(err));
+            var m = metalsmith.metadata();
+            assert.notEqual(m.articles,null);
+            assert.notEqual(m.collections.articles,null);
+            assert.equal(m.secrets,null);
+            assert.equal(m.collections.secrets,null);
+            done();
+        });
+    });
 
-	it('should retain private collections with private scope option', function(done) {
-		var metalsmith = Metalsmith('test/fixtures/basic');
-		metalsmith
-		  .use(collections({
-        articles: {},
-        secrets: {
-          metadata: { private: true }
-        }
-      }))
-      .use(collectionScoping({scope: 'private'}))
-		  .build(function(err) {
-		  	if (err) return(done(err));
-		  	var m = metalsmith.metadata();
-		  	assert.notEqual(m.articles,null);
-		  	assert.notEqual(m.collections.articles,null);
-		  	assert.notEqual(m.secrets,null);
-		  	assert.notEqual(m.collections.secrets,null);
-		  	done();
-		  });
-	});
+    it('should retain private collections with private scope option', function(done) {
+        var metalsmith = Metalsmith('test/fixtures/basic');
+        metalsmith
+        .use(collections({
+            articles: {},
+            secrets: {
+                metadata: { private: true }
+            }
+        }))
+        .use(collectionScoping({scope: 'private'}))
+        .build(function(err) {
+            if (err) return(done(err));
+            var m = metalsmith.metadata();
+            assert.notEqual(m.articles,null);
+            assert.notEqual(m.collections.articles,null);
+            assert.notEqual(m.secrets,null);
+            assert.notEqual(m.collections.secrets,null);
+            done();
+        });
+    });
 
-	it("should add 'private' metadata for files in private collections", function(done) {
-		var metalsmith = Metalsmith('test/fixtures/basic');
-		metalsmith
-		  .use(collections({
-        articles: {},
-        secrets: {
-          metadata: { private: true }
-        }
-      }))
-      .use(collectionScoping({propagate: true}))
-      .build(function(err, files) {
-      	assert.equal(files['three.md'].private, true);
-      	assert.equal(files['four.md'].private, true);
-      	assert.equal(files['one.md'].private, undefined);
-      	assert.equal(files['five.md'].private, undefined);
-      	done();
-      });
-	});
-	
+    it("should add 'private' metadata for files in private collections", function(done) {
+        var metalsmith = Metalsmith('test/fixtures/basic');
+        metalsmith
+        .use(collections({
+            articles: {},
+            secrets: {
+                metadata: { private: true }
+            }
+        }))
+        .use(collectionScoping({propagate: true}))
+        .build(function(err, files) {
+            assert.equal(files['three.md'].private, true);
+            assert.equal(files['four.md'].private, true);
+            assert.equal(files['one.md'].private, undefined);
+            assert.equal(files['five.md'].private, undefined);
+            done();
+        });
+    });
 });
